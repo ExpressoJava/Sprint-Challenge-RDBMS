5cef02255210b264c0c986ff460b319347b535e4
Bao Pham <ryanvuepham@gmail.com>
2019-03-23 11:49:43 -0500

    completed migrations

---
+++ data/migrations/20190323114225_projects.js
@@ -0,0 +1,17 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('projects', tbl => {
+      tbl.increments()
+      tbl
+          .string('project', 128)
+          .notNullable()
+      tbl
+          .string('description')
+      tbl
+          .boolean('Completed')
+          .defaultTo(0)
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('projects')
+}
