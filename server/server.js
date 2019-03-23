c9c4634f346bc362be6c8eb8f69ffb13a53fca51
Bao Pham <ryanvuepham@gmail.com>
2019-03-23 13:24:40 -0500

    actions routes working

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const server = express()
 server.use(express.json())
 
 server.use('/api/projects', projectRouter)
-//server.use('/api/actions', actionRouter)
+server.use('/api/actions', actionRouter)
 
 
 
